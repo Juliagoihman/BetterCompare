5496d81a89a8b97a3250a5c097aaff02fa43aa5b
Julia Goihman <87q5h78tj4@privaterelay.appleid.com>
2026-04-29 01:33:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Start verticals in background
-uvicorn proxy.verticals.internet.main:app --port 8801 &
-uvicorn proxy.verticals.mobile.main:app --port 8802 &
-uvicorn proxy.verticals.travel.main:app --port 8803 &
+cd /app/proxy
+uvicorn verticals.internet.main:app --port 8801 &
+uvicorn verticals.mobile.main:app --port 8802 &
+uvicorn verticals.travel.main:app --port 8803 &
 
 # Wait for verticals to start
 sleep 2
 
 # Start proxy
-uvicorn proxy.main:app --host 0.0.0.0 --port 8787
+uvicorn main:app --host 0.0.0.0 --port 8787
