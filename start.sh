bd94a7d0b0e032ce4effd2375af3f62d0edb75a6
Julia Goihman <87q5h78tj4@privaterelay.appleid.com>
2026-04-30 15:47:37 +0200

    Add insurance vertical to start.sh
    Added insurance vertical to the startup script.

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Start verticals in background
 cd /app/proxy
+
 uvicorn verticals.internet.main:app --port 8801 &
 uvicorn verticals.mobile.main:app --port 8802 &
 uvicorn verticals.travel.main:app --port 8803 &
+uvicorn verticals.insurance.main:app --port 8804 &
 
-# Wait for verticals to start
 sleep 2
 
-# Start proxy
 uvicorn main:app --host 0.0.0.0 --port 8787
